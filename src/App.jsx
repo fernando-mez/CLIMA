275235aee820693a63a14b9acefbd1d9a1fb34f0
fernando-mez <mezquita907@gmail.com>
2022-10-31 22:03:45 -0400

    se agrega el proyecto

---
+++ src/App.jsx
@@ -0,0 +1,76 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import useGeoLocation from './hooks/useGeoLocation'
+import useOpenWheater from './hooks/useOpenWheater'
+import useGoogleMaps from './hooks/useGoogleMaps';
+import imagesWheather from './assets/imagesWheather';
+import imgPermiso from './assets/permiso.png'
+
+function App() {
+  const {GeoEstate, Latitude, Longitude, Accuracy, accessGeo} = useGeoLocation(); 
+  const {ObjOpenWheater} = useOpenWheater(Latitude,Longitude);
+  const {ObjGoogleMaps} = useGoogleMaps(Latitude,Longitude);
+  const [IsCelsiusOrFahrenheit,setIsCelsiusOrFahrenheit] = useState('F');
+  const [Grades,setGrades] = useState(0);
+
+  const changeUnitTemperature = () => {
+    if(IsCelsiusOrFahrenheit === 'C') {
+      setIsCelsiusOrFahrenheit('F');
+      setGrades(Math.round((((ObjOpenWheater.main?.temp-273.15)*(9/5)) + 32 + Number.EPSILON) * 100) / 100);
+    }
+    if(IsCelsiusOrFahrenheit === 'F') {
+      setIsCelsiusOrFahrenheit('C');
+      setGrades(Math.round(((ObjOpenWheater.main?.temp-273.15) + Number.EPSILON) * 100) / 100);
+    }
+  }
+
+  useEffect(()=>{  
+    document.body.style.backgroundImage = "url('"+imagesWheather["img_"+ObjOpenWheater.weather?.[0].icon]+"')";
+    if(Object.keys(ObjOpenWheater).length !== 0 && Object.keys(ObjGoogleMaps).length !== 0){      
+      changeUnitTemperature();   
+    }
+  },[ObjOpenWheater, ObjGoogleMaps]);
+
+
+    return (
+    <>
+   
+      <div className='card' style={GeoEstate?{display:'block'}:{display:'none'}}> 
+          <h1>Wheather App</h1>
+          <h2>
+            {
+            `${ObjGoogleMaps?.address_components?.[2].long_name}, 
+            ${ObjGoogleMaps?.address_components?.[3].long_name}, 
+            ${ObjGoogleMaps?.address_components?.[5].long_name},
+            ${ObjGoogleMaps?.address_components?.[6].long_name}`
+            }
+          </h2>
+          <div className='data-wheather'>
+              <div className='temperature'>
+                {
+                  (ObjOpenWheater.weather?.[0].icon!==undefined)
+                  ?  (<img src={"https://openweathermap.org/img/wn/"+ObjOpenWheater.weather?.[0].icon+"@4x.png"} alt="" width="200" height="200" />)
+                  :  null
+                }             
+                <h2>{Grades}{" "}{IsCelsiusOrFahrenheit}º</h2>
+              </div>
+              <div className='other-data'>
+                <h2>Scattered clouds</h2>
+                <p><i className="fa-solid fa-wind"></i> Wind Speed:   {ObjOpenWheater.wind?.speed} m/s</p>
+                <p><i className="fa-solid fa-cloud"></i> Clouds:      {ObjOpenWheater.clouds?.all}%</p>
+                <p><i className="fa-solid fa-temperature-half"></i> Pressure: {ObjOpenWheater.main?.pressure} mb</p>
+                <p><i className="fa-brands fa-drupal"></i> Humidity:            {ObjOpenWheater.main?.humidity}%</p>
+              </div>
+          </div>
+          <button onClick={changeUnitTemperature} className="buttonDegrees">Degrees Fº / Cº</button>
+      </div>
+      <div className='card' style={GeoEstate?{display:'none'}:{display:'block'}}>
+          <h1>Sin Acceso a Geolocalizacion</h1>
+          <p>Debe restablecer el permiso de Geolocalizacion y Actualziar la Pagina.</p>
+          <img src={imgPermiso} alt="" />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/assets/imagesWheather.js
@@ -0,0 +1,38 @@
+import img_01d from './img/back-01d.jpg'
+import img_01n from './img/back-01n.jpg'
+import img_02d from './img/back-02d.webp'
+import img_02n from './img/back-02n.jpg'
+import img_03d from './img/back-03d.jpg'
+import img_03n from './img/bridge-screen.jpg'
+import img_04d from './img/back-04d.jpg'
+import img_04n from './img/rock-screen.jpg'
+import img_09d from './img/back-09d.jpg'
+import img_09n from './img/back-09n.jpg'
+import img_10d from './img/back-10d.jpg'
+import img_10n from './img/back-10n.jpg'
+import img_11d from './img/back-11d.jpg'
+import img_11n from './img/back-11n.jpg'
+import img_13d from './img/back-13d.jpg'
+import img_13n from './img/back-13n.jpg'
+import img_50d from './img/back-50d.jpg'
+import img_50n from './img/street-screen.jpg'   
+export default {
+    img_01d:img_01d,
+    img_01n:img_01n,
+    img_02d:img_02d,
+    img_02n:img_02n,
+    img_03d:img_03d,
+    img_03n:img_03n,
+    img_04d:img_04d,
+    img_04n:img_04n,
+    img_09d:img_09d,
+    img_09n:img_09n,
+    img_10d:img_10d,
+    img_10n:img_10n,
+    img_11d:img_11d,
+    img_11n:img_11n,
+    img_13d:img_13d,
+    img_13n:img_13n,
+    img_50d:img_50d,
+    img_50n:img_50n,
+}
